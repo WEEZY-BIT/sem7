d0910ff908e3e73dc57445dd57ef317c9087e447
Radik <radik.wood@yandex.ru>
2023-08-28 23:16:41 +0300

    выбрал файлы

---
+++ zadacha50/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+int[,] myArray = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+
+Console.Write("Введите номер строки: ");
+int rowInput = int.Parse(Console.ReadLine());
+
+Console.Write("Введите номер столбца: ");
+int columnInput = int.Parse(Console.ReadLine());
+
+int result = GetElementValue(myArray, rowInput, columnInput);
+if (result == -1)
+{
+    Console.WriteLine("Такого элемента нет");
+}
+else
+{
+    Console.WriteLine($"Значение элемента: {result}");
+}
+    
+
+    static int GetElementValue(int[,] array, int row, int column)
+{
+    if (row < 0 || column < 0 || row >= array.GetLength(0) || column >= array.GetLength(1))
+    {
+        return -1;
+    }
+    else
+    {
+        return array[row, column];
+    }
+}
---
+++ zadacha52/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+int[,] myArray = {{ 1, 2, 3 },{ 4, 5, 6 },{ 7, 8, 9 }};
+
+int rows = myArray.GetLength(0);
+int columns = myArray.GetLength(1);
+
+double[] columnAverages = new double[columns];
+
+for (int column = 0; column < columns; column++)
+{
+    int sum = 0;
+    for (int row = 0; row < rows; row++)
+    {
+        sum += myArray[row, column];
+    }
+    columnAverages[column] = (double)sum / rows;
+}
+
+Console.WriteLine("Средние арифметические значения столбцов:");
+for (int column = 0; column < columns; column++)
+{
+    Console.WriteLine($"Столбец {column + 1}: {columnAverages[column]}");
+}
